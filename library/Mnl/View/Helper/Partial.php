f360be848c0f7f82ef257c8e0ec8a42300d5d44b
Markus Nilsson <markus@mnilsson.se>
2011-11-23 00:43:12 +0100

    View helpers should use namespaces

--- library/Mnl/View/Helper/Partial.php
+++ library/Mnl/View/Helper/Partial.php
@@ -8,17 +8,18 @@
 * @license  http://www.opensource.org/licenses/mit-license.php MIT Licence
 * @link     http://mnilsson.se/Mnl
 */
-class Mnl_View_Helper_Partial extends Mnl_View_Helper_Abstract
+namespace Mnl\View\Helper;
+class Partial extends \Mnl\View\HelperAbstract
 {
     public static function run($args)
     {
         $file = $args[0];
         if (isset($args[1])) {
-            $data = $args[1];             
+            $data = $args[1];
         } else {
             $data = array();
         }
-        $partial = new Mnl_View();
+        $partial = new \Mnl\View();
         $partial->assign($data);
         return $partial->fetch($file);
     }
